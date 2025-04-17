36d8e25214ea955da4dcb0b7f4d6feaf66ee06ff
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-04-17 11:27:11 +0200

    Add header files for horde and zombie classes

---
+++ ex01/include/include.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 11:16:26 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/04/17 11:16:27 by daniel-esca      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_HPP
+# define INCLUDE_HPP
+
+#include <iostream>
+#include "../horde.hpp"
+
+#endif
