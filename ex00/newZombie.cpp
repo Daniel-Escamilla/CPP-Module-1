a93f1c6e9ab8532cb0e38f286172f39a01012221
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-04-18 19:06:48 +0200

    Add newZombie function to create a Zombie instance

---
+++ ex00/newZombie.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 17:00:18 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/04/17 17:00:33 by daniel-esca      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie	*newZombie(std::string name)
+{
+	Zombie	*new_zombie;
+
+	new_zombie = new Zombie(name);
+	return (new_zombie);
+}
