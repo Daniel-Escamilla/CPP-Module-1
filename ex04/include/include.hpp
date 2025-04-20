ec8125f42a8f8190c65d7e16aba96d3a789b2c2d
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-20 20:13:25 +0200

    Add include.hpp header file with necessary includes and guards

---
+++ ex04/include/include.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 19:02:15 by descamil          #+#    #+#             */
+/*   Updated: 2025/04/20 19:09:10 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_HPP
+# define INCLUDE_HPP
+
+# include <iostream>
+# include <string>
+# include <fstream>
+# include <cstdlib>
+# include <stdexcept>
+# include "replace.hpp"
+
+#endif
