7afa4bafacd83aa7e390f5c375a1c327c1402c77
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-20 17:48:24 +0200

    Add HumanB.cpp implementation file with constructor, destructor, setWeapon, and attack methods

---
+++ ex03/src/HumanB.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanB.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 20:06:16 by descamil          #+#    #+#             */
+/*   Updated: 2025/04/19 19:32:08 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/include.hpp"
+
+HumanB::HumanB(std::string new_name)
+{
+	name = new_name;
+}
+
+HumanB::~HumanB()
+{
+	
+}
+
+void		HumanB::setWeapon(Weapon &arm)
+{
+	weapon = &arm;
+}
+void		HumanB::attack()
+{
+	std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+}
