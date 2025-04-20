34b2f7507e08bda2e244d42c5186b2894faf7d1d
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-20 17:48:28 +0200

    Add main.cpp file with main function demonstrating HumanA and HumanB interactions

---
+++ ex03/src/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 20:06:13 by descamil          #+#    #+#             */
+/*   Updated: 2025/04/19 19:22:00 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/include.hpp"
+
+int main()
+{
+	{
+		Weapon club = Weapon("crude piked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude piked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return (0);
+}
