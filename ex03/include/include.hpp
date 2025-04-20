37b574a00561cea3c270764690bad2c495f22cce
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-20 17:48:07 +0200

    Add include.hpp header file with necessary includes for Weapon and Human classes

---
+++ ex03/include/include.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 20:06:43 by descamil          #+#    #+#             */
+/*   Updated: 2025/04/19 18:45:47 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_HPP
+# define INCLUDE_HPP
+
+# include <iostream>
+# include <string>
+# include "Weapon.hpp"
+# include "HumanA.hpp"
+# include "HumanB.hpp"
+
+#endif
