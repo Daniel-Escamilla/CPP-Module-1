f9860fe3eb954d7ad8d9014122b8aba8e0a4ab29
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-21 18:30:57 +0200

    Add include.hpp header file for HarlFilter class

---
+++ ex06/include/include.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 17:58:44 by descamil          #+#    #+#             */
+/*   Updated: 2025/04/21 17:58:45 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_HPP
+# define INCLUDE_HPP
+
+# include <iostream>
+# include <string>
+# include "harlFilter.hpp"
+
+#endif
